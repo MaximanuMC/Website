a572018a4a98097e81e9038f294da6465f4c3fbc
Maximanu <151451667+MaximanuMC@users.noreply.github.com>
2025-01-30 07:42:33 +0100

    Update page.jsx

--- app/revalidation/page.jsx
+++ app/revalidation/page.jsx
@@ -12,28 +12,15 @@ const maxExtractLength = 200;
 const revalidateTTL = 60;
 
 const explainer = `
-This page perfoms a \`fetch\` on the server to get a random article from Wikipedia. 
-The fetched data is then cached with a tag named "${tagName}" and a maximum age of ${revalidateTTL} seconds.
+Hallo! Icv bin Maxi und hier ein paar Informationen über mich:
 
 ~~~jsx
-const url = 'https://en.wikipedia.org/api/rest_v1/page/random/summary';
-
-async function RandomArticleComponent() {
-    const randomArticle = await fetch(url, {
-        next: { revalidate: ${revalidateTTL}, tags: ['${tagName}'] }
-    });
-    // ...render
-}
+Name: Maxi
+Land: Deutschland
 ~~~
 
-After the set time has passed, the first request for this page would trigger its rebuild in the background. When the new page is ready, subsequent requests would return the new page - 
-see [\`stale-white-revalidate\`](https://www.netlify.com/blog/swr-and-fine-grained-cache-control/).
-
-Alternatively, if the cache tag is explicitly invalidated by \`revalidateTag('${tagName}')\`, any page using that tag would be rebuilt in the background when requested.
-
-In real-life applications, tags are typically invalidated when data has changed in an external system (e.g., the CMS notifies the site about content changes via a webhook), or after a data mutation made through the site.
-
-For this functionality to work, Next.js uses the [fine-grained caching headers](https://docs.netlify.com/platform/caching/) available on Netlify - but you can use these features on basically any Netlify site!
+Ich benutze oft Wikipedia und finde es gut, dass allen Leuten so viel gratis Wissen bereitgestellt wird.
+Hier ist ein zufälliger Wikipedia Artikel:
 `;
 
 
