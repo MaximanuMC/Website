7aa737e8211e105c72d29d67a7f4f94a54889304
Maximanu <151451667+MaximanuMC@users.noreply.github.com>
2025-01-30 15:15:51 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -17,7 +17,6 @@ export default function Page() {
     return (
         <main className="flex flex-col gap-8 sm:gap-16">
             <section className="flex flex-col items-start gap-3 sm:gap-4">
-                <ContextAlert />
                 <h1 className="mb-0">Willkommen auf der Website von Maximanu SMP!</h1>
                 <p className="text-lg">Wir sind ein deutsches Minecraft Server Netzwerk.</p>
                 <Link
