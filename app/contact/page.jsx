8124361c2f2ccaf61cdbc72ba48e114c08cfd33a
Maximanu <151451667+MaximanuMC@users.noreply.github.com>
2025-01-31 07:48:35 +0100

    Create page.jsx

---
+++ app/contact/page.jsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import { Markdown } from 'components/markdown';
+
+export const metadata = {
+    title: 'Maximanu SMP'
+};
+
+const explainer = `
+Es gibt drei verschiedene Spielmodi:
+• Überleben
+• Skyblock
+• Factions
+
+Viel Spaß auf Maximanu SMP
+`;
+
+export default function Page() {
+    return (
+        <main className="flex flex-col gap-8 sm:gap-16">
+            <section className="flex flex-col items-start gap-3 sm:gap-4">
+                <h1 className="mb-0">Willkommen auf der Website von Maximanu SMP!</h1>
+                <p className="text-lg">Wir sind ein deutsches Minecraft Server Netzwerk.</p>
+                <Link
+                    href="/play"
+                    className="btn btn-lg btn-primary sm:btn-wide"
+                >
+                    Jetzt spielen!
+                </Link>
+                <Markdown content={explainer} />
+            </section>
+        </main>
+    );
+}
