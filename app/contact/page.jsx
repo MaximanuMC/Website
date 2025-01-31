033bb19dfd4c6e14b649f03536edd7f185c1a912
Maximanu <151451667+MaximanuMC@users.noreply.github.com>
2025-01-31 13:34:13 +0100

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,30 +2,23 @@ import Link from 'next/link';
 import { Markdown } from 'components/markdown';
 
 export const metadata = {
-    title: 'Maximanu SMP'
+    title: 'Kontakt | Maximanu SMP'
 };
 
 const explainer = `
-Es gibt drei verschiedene Spielmodi:
-• Überleben
-• Skyblock
-• Factions
+Hier sind unsere Kontaktdaten:
 
-Viel Spaß auf Maximanu SMP
+~~~jsx
+E-Mail-Adresse: maxi@maximanu.de
+bald mehr Möglichkeiten...
+~~~
 `;
 
 export default function Page() {
     return (
         <main className="flex flex-col gap-8 sm:gap-16">
             <section className="flex flex-col items-start gap-3 sm:gap-4">
-                <h1 className="mb-0">Willkommen auf der Website von Maximanu SMP!</h1>
-                <p className="text-lg">Wir sind ein deutsches Minecraft Server Netzwerk.</p>
-                <Link
-                    href="/play"
-                    className="btn btn-lg btn-primary sm:btn-wide"
-                >
-                    Jetzt spielen!
-                </Link>
+                <h1 className="mb-0">Kontakt</h1>
                 <Markdown content={explainer} />
             </section>
         </main>
