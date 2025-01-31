ce24f9876f3040d12c60473d42c7f1418665c199
Maximanu <151451667+MaximanuMC@users.noreply.github.com>
2025-01-31 07:47:11 +0100

    Update page.jsx

--- app/wiki/page.jsx
+++ app/wiki/page.jsx
@@ -1,31 +1,19 @@
 import Link from 'next/link';
-import { Markdown } from '../../components/markdown';
+import { Markdown } from 'components/markdown';
 
 export const metadata = {
-    title: 'Maximanu SMP'
+    title: 'Wiki | Maximanu SMP'
 };
 
 const explainer = `
-Es gibt drei verschiedene Spielmodi:
-• Überleben
-• Skyblock
-• Factions
-
-Viel Spaß auf Maximanu SMP
+Noch kein Wiki.
 `;
 
 export default function Page() {
     return (
         <main className="flex flex-col gap-8 sm:gap-16">
             <section className="flex flex-col items-start gap-3 sm:gap-4">
-                <h1 className="mb-0">Willkommen auf der Website von Maximanu SMP!</h1>
-                <p className="text-lg">Wir sind ein deutsches Minecraft Server Netzwerk.</p>
-                <Link
-                    href="/play"
-                    className="btn btn-lg btn-primary sm:btn-wide"
-                >
-                    Jetzt spielen!
-                </Link>
+                <h1 className="mb-0">Wiki</h1>
                 <Markdown content={explainer} />
             </section>
         </main>
